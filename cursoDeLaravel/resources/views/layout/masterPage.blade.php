e11a3bbab075eecb75c7f1a29375b3da98344f83
LiviaNascimento <liviapriscilla1989@gmail.com>
2019-02-06 15:09:03 +0000

    ola
    Co-Authored-By: Lívia Nascimento <liviapriscilla1989@gmail.com>

--- cursoDeLaravel/resources/views/layout/masterPage.blade.php
+++ cursoDeLaravel/resources/views/layout/masterPage.blade.php
@@ -1,15 +1,4 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-        <title>@yield('titulo')</title>
-
-
-      </head>
-<body>
+    @include('layout._includes.header')
     @yield('content')
-</body>
-  </html>
+    @include('layout._includes.footer')
